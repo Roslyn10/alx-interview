cbd6a040edee647e9885e59c7245552c6c02c38a
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 21:27:52 +0200

    Adding the code for 0-lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" A function that determines whether boxes can be opened using a key"""
+
+def canUnlockAll(boxes):
+
+    keys = set()
+    unlocked = set()
+    queue = [0]
+    unlocked.add(0)
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if key < len(boxes) and key not in unlocked:
+                unlocked.add(key)
+                queue.append(key)
+
+    return len(unlocked) == len(boxes)
