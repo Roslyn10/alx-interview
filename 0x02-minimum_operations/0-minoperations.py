c7548521321d7240eaee705040dc7ee2f4287aa5
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-23 21:25:53 +0200

    Fixing the pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,29 @@
+#!/usr/bin/pythin3
+"""A function that calculates the minimum operations"""
+
+
 def minOperations(n):
+    """
+    Calaculates the minimun number of operations it
+    takes to result in n H characters
+
+    Args:
+        n (int): The given number of operations needed
+
+    Return:
+        N number of operations
+    """
+
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
