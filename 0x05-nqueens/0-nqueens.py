8f0cc194c5ac7bae0ebdcf86cb341144d4b5a5d8
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-11 16:10:22 +0200

    Adding the exceptions and error messages to the code

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -45,13 +45,17 @@ class Solution:
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: 0-nqueens.py <n>")
+        print("Usage: nqueens N")
         sys.exit(1)
     try:
         n = int(sys.argv[1])
-    except ValueError:
-        print("Error: n must be an integer.")
+    except Exception:
+        print("N must be a number")
         sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return n
 
     sol = Solution()
     output = sol.NQueens(n)
