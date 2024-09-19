85427bda2c263fd0439090d59278d620a65b89fe
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-19 22:38:06 +0200

    Fixing the code from javascript to node js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,46 +2,30 @@
 
 const request = require('request');
 
-// Get the movie ID from the command line arguments
 const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-// Check if the movie ID was provided
-if (!movieId) {
-  console.error('Please provide a movie ID as the first argument.');
-  process.exit(1);
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
 }
 
-// Star Wars API URL for the movie
-const movieEndpoint = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Make a request to get the movie details
-request(movieEndpoint, { json: true }, (error, response, body) => {
+request(movieEndpoint, (error, response, body) => {
   if (error) {
-    console.error('Error fetching movie data:', error);
-    return;
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Failed to retrieve movie data. Status code:', response.statusCode);
-    return;
-  }
-
-  // Extract the characters list from the movie data
-  const characterUrls = body.characters;
-
-  // Iterate over the character URLs and print each character's name
-  characterUrls.forEach((url) => {
-    request(url, { json: true }, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error fetching character data:', charError);
-        return;0-starwars_characters.js
-      }
-
-      if (charResponse.statusCode === 200) {
-        console.log(charBody.name);
-      } else {
-        console.error('Failed to retrieve character data. Status code:', charResponse.statusCode);
-      }
-    });
-  });
 });
