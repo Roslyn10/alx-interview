64ec0d4e214c8160f0da51b7274b6398e09c0e29
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-15 16:32:06 +0200

    Seperating the two functions, and fixing the logic in the help function that generates prime numbers

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,34 +1,47 @@
 #!/usr/bin/python3
-"""Prime game"""
+"""
+Module: Game of choosing Prime numbers
+"""
+
+
+def generate_primes(n):
+    """
+    Helper function that generates numbers and
+    checks if they are prime numbers
+
+    Args:
+        is_prime: empty list
+        fil_prime: a list of numbers that are set to true
+
+    Return:
+        returns the prime numbers
+    """
+    is_prime = []
+    fil_prime = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (fil_prime[prime]):
+            is_prime.append(prime)
+            for i in range(prime, n + 1, prime):
+                fil_prime[i] = False
+    return is_prime
 
 
 def isWinner(x, nums):
-    """"
-    Main function that simulates the game play
     """
-    def generate_primes(n):
-        """
-        Helper function that generates numbers and
-        checks if they are prime numbers
-        """
-        is_prime = [True for i in range(n + 1)]
-        is_prime[0] = is_prime[1] = False
-
-        p = 2
-        while (p * p <= n):
-            if is_prime[p]:
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-            p += 1
-
-        return [p for p in range(2, n + 1) if is_prime[p]]
-
-    Maria = 0
-    Ben = 0
-
-    for i in range(x):
-        prime = generate_primes(nums[i])
-        if len(prime) % 2 == 0:
+    Determines winner of Prime Game
+    Args:
+        x (int): number  of rounds per game
+        nums (int): the nth number in the game
+    Return:
+        Name of the Winner either
+        Ben or Maria
+    """
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    Maria = Ben = 0
+    for p in range(x):
+        is_prime = generate_primes(nums[p])
+        if len(is_prime) % 2 == 0:
             Ben += 1
         else:
             Maria += 1
